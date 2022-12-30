ea420c2fb4fd01441bcf5c661b4a199b1d275fc8
jaehoon-sim <69947834+jaehoon-sim@users.noreply.github.com>
2022-12-30 13:06:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ from datetime import datetime, timezone
 
 
 newTime = datetime.now()
-print(newTime)
+# print(newTime)
 dtString = newTime.strftime("%y_%m_%d_%H oclock")
 
-url = 'https://www.melon.com/chart/index.htm'
+period = ['day/', 'week/', 'month/'] #일간, 주간, 월간
+
+
+url = 'https://www.melon.com/chart/'+period[1]+'index.htm'
+# print(url)
 req_header_dict = {
     # 요청헤더 : 브라우저정보
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'
@@ -38,7 +42,7 @@ for idx, a_tag in enumerate(a_tags,1):
     song_dict = {}    
     #노래 제목 갖고오기 <a href="">노래제목</a>
     song_title = a_tag.text
-    song_dict['rank'] = idx
+    # song_dict['rank'] = idx
     song_dict['song_title'] = song_title
     # print(song_dict)
     
@@ -52,12 +56,51 @@ for idx, a_tag in enumerate(a_tags,1):
     if matched:
         song_id = matched.group(1) # group(0) : 34535898);  group(1) : 34535898
         song_dict['song_id'] = song_id
-        print(song_title +" / " + song_id)
         
         song_detail_url = f'https://www.melon.com/song/detail.htm?songId={song_id}'
         song_dict['song_detail_url'] = song_detail_url
         song_list.append(song_dict)
         # print(song_list)
 
+song_detail_list = []
+for idx, song in enumerate(song_list,1): # 결과가 매번 100개씩출력되어서 3순위까지 임시로 슬라이싱
+    #노래 1곡의 상세정보를 저장할 dict
+    song_detail_dict = {}
+    song_detail_url = song['song_detail_url']
+    # print(song_detail_url)
+
+    
+    res = requests.get(song_detail_url, headers=req_header_dict)
+    #print(res.status_code)
+    if res.ok:
+        soup = BeautifulSoup(res.text, 'html.parser')
+        #print(idx,song['song_title'])
+        song_detail_dict['곡명'] = song['song_title']
+        #노래 1곡의 상세정보에 '곡명'을 찾아 주었다. 
+        #다음으로 상세정보 '가수'를 찾는다
+        singer = soup.select("a[href*='goArtistDetail'] span")
+        #print(singer)
+        if singer:
+            song_detail_dict['가수'] = singer[0].text # singer리스트에서 0번째 순서에 해당하는것을 text로 뽑으면 이름이다.
+        #print(singer[0].text)
+        
+        #다음으로 div.meta에서 dd로 끝나는 값들을 넣어준다
+        song_di = soup.select("div.meta dd")
+        song_coverImg = soup.select_one("#downloadfrm > div > div > div.thumb > a > img")['src']
+        
+        if song_di:
+            song_detail_dict['앨범'] = song_di[0].text
+            song_detail_dict['발매일'] = song_di[1].text
+            song_detail_dict['장르'] = song_di[2].text
+            song_detail_dict['FLAC'] = song_di[3].text
+            
+        song_detail_dict['재킷'] = song_coverImg
+                    
+        song_detail_list.append(song_detail_dict)
+        
+        # if song_coverImg:
+        #     song_detail_dict['앨범재킷'] = song_coverImg[0].text
+# print(song_detail_list)
+
 with open('melonTop100.json','w',encoding='utf-8') as file:
-	json.dump(song_list, file, ensure_ascii=False)
+	json.dump(song_detail_list, file, ensure_ascii=False)
